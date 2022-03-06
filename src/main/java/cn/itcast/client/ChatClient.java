67e290dc36b6eedc5e1337336ff1c46e42c29e6a
zeyuan <zeyuan@mogu.com>
2022-03-06 22:53:35 +0800

    初始化项目

---
+++ src/main/java/cn/itcast/client/ChatClient.java
@@ -0,0 +1,45 @@
+package cn.itcast.client;
+
+import cn.itcast.protocol.MessageCodecSharable;
+import cn.itcast.protocol.ProcotolFrameDecoder;
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class ChatClient {
+    public static void main(String[] args) {
+        NioEventLoopGroup group = new NioEventLoopGroup();
+        LoggingHandler LOGGING_HANDLER = new LoggingHandler(LogLevel.DEBUG);
+        MessageCodecSharable MESSAGE_CODEC = new MessageCodecSharable();
+        try {
+            Bootstrap bootstrap = new Bootstrap();
+            bootstrap.channel(NioSocketChannel.class);
+            bootstrap.group(group);
+            bootstrap.handler(new ChannelInitializer<SocketChannel>() {
+                @Override
+                protected void initChannel(SocketChannel ch) throws Exception {
+                    //ProcotolFrameDecoder每个channel独有，用于半包黏包解决，必须独有以记录状态，如果是单例的话不同channel会有影响
+                    ch.pipeline().addLast(new ProcotolFrameDecoder());
+                    ch.pipeline().addLast(LOGGING_HANDLER);
+                    ch.pipeline().addLast(MESSAGE_CODEC);
+                    //连接建立触发active事件
+//                    ch.pipeline().addLast()
+
+                }
+            });
+            Channel channel = bootstrap.connect("localhost", 8080).sync().channel();
+            channel.closeFuture().sync();
+        } catch (Exception e) {
+            log.error("client error", e);
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
